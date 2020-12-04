76007dd0803315ec93b46f96b096f4fc50144a32
Jiwoo Jeon <jjw2995@gmail.com>
2020-12-03 18:14:30 -0800

    adding log points

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -1,41 +1,44 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyAccessToken = function (req, res, next) {
-	let msg = 'invalid access token, refresh token';
-	const authHeader = req.headers['authorization'];
-	const token = authHeader && authHeader.split(' ')[1];
-	// Bearer <access token>
-	// if (token == null) return res.status(401).json({ error: msg });
-	// console.log(token);
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		if (err) return res.status(401).json({ error: msg });
-		delete user.iat;
-		delete user.exp;
-		// console.log(user);
-		req.user = user;
-		next();
-	});
+  let msg = "invalid access token, refresh token";
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  // Bearer <access token>
+  // if (token == null) return res.status(401).json({ error: msg });
+  // console.log(token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.log("accessTok verify fail");
+      return res.status(401).json({ error: msg });
+    }
+    delete user.iat;
+    delete user.exp;
+    // console.log(user);
+    req.user = user;
+    next();
+  });
 };
 
 const verifyRefreshToken = function (req, res, next) {
-	let msg = 'invalid refresh token, log in again';
-	let token = req.body.refreshToken;
-	// if (token == null) return res.status(401).json({ error: msg });
+  let msg = "invalid refresh token, log in again";
+  let token = req.body.refreshToken;
+  // if (token == null) return res.status(401).json({ error: msg });
 
-	jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-		if (err) return res.status(401).json({ error: msg });
-		delete user.iat;
-		delete user.exp;
-		// console.log(user);
-		user.refreshToken = token;
-		req.user = user;
-		next();
-	});
+  jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(401).json({ error: msg });
+    delete user.iat;
+    delete user.exp;
+    // console.log(user);
+    user.refreshToken = token;
+    req.user = user;
+    next();
+  });
 };
 
 module.exports = {
-	verifyAccessToken,
-	verifyRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
 };
 
 // const model = require('mongoose').model('Follower');
